928683dd200cd01d09bf8047b7fdeb943721b781
unknown <abhim3434@hotmail.com>
2019-01-16 23:44:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/arihantas/skeleton/Activity/PostActivity.java
@@ -0,0 +1,15 @@
+package com.arihantas.skeleton.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arihantas.skeleton.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
---
+++ app/src/main/java/com/arihantas/skeleton/Activity/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.arihantas.skeleton.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arihantas.skeleton.R;
+
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
